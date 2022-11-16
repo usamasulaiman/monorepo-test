c570a4931586fe085fd6db19bd77ed5e94a493cb
Usama Suliaman <sulaiman.usama@gmail.com>
2022-11-16 17:59:23 +0100

    back to indie stack

--- remix-core/tests/example.spec.ts
+++ remix-core/tests/example.spec.ts
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 // const baseUrl = `http://localhost:${process.env.PORT}`;
 
 test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://github.com/remix-run');
+  await page.goto('https://github.com/remix-run/indie-stack');
   await expect(page).toHaveScreenshot();
 
   // Expect a title "to contain" a substring.
